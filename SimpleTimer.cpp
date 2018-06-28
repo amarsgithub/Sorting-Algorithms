0deb1d2b3e56a6694aac8d037a4573d3ea35c9fd
Amar <amarsgithub@outlook.com>
2018-06-27 23:05:37 -0400

    first commit

---
+++ SimpleTimer.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include "SimpleTimer.h"
+using namespace std;
+
+SimpleTimer::SimpleTimer()
+{
+    // Get the current time
+    start = highResClock::now();
+}
+
+SimpleTimer::SimpleTimer(string name)
+{
+    // Get the current time
+    this->name = name;
+    start = highResClock::now();
+}
+
+SimpleTimer::~SimpleTimer()
+{
+    end = highResClock::now();
+    elapsed = end - start;
+    cout << "Elapsed time " << name << " "<< elapsed.count() << " seconds\n";
+}
